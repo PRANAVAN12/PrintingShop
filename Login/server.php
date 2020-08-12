75466012b7251e983102c2c89082b5f6f872ca31
PRANAVAN12 <npranavan28@gmail.com>
2020-08-12 15:37:44 +0530

    changes

--- Login/server.php
+++ Login/server.php
@@ -35,7 +35,7 @@
 
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
-			$password = md5($password_1);//encrypt the password before saving in the database
+			//$password = md5($password_1);//encrypt the password before saving in the database
 			$query = "INSERT INTO users (UserName,Email,Mobile, password) 
 					  VALUES('$UserName', '$Email', '$Mobile', '$password')";
 			mysqli_query($db, $query);
@@ -62,7 +62,7 @@
 		}
 
 		if (count($errors) == 0) {
-			$password = md5($password);
+			//$password = md5($password);
 			$query = "SELECT * FROM users WHERE UserName='$UserName' AND password='$password'";
 			$results = mysqli_query($db, $query);
 
--- Orders/index.php
+++ Orders/index.php
@@ -9,33 +9,74 @@ $result = mysqli_query($mysqli, "SELECT * FROM orders ORDER BY id DESC"); // usi
 
 <html>
 <head>	
-	<title>Homepage</title>
+	<title></title>
+	<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="../table/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../table/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../table/css/main.css">
+<!--===============================================================================================-->
 </head>
 <a href="../Admin.php">Home</a>
 <body>
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver1 m-b-110">
+					<div class="table100-head">
+						<table>
+							<thead>
+								<tr class="row100 head">
+									<th class="cell100 column1">Categary</th>
+									<th class="cell100 column2">quantity</th>
+									<th class="cell100 column3">description</th>
+									
+								
+								</tr>
+							</thead>
+						</table>
+					</div>
+
+					<div class="table100-body js-pscroll">
+						<table>
+							<tbody>
+							<?php 
+//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+while($res = mysqli_fetch_array($result)) { 		
+  echo "<tr>";
+  echo "<td>".$res['categary']."</td>";
+  echo "<td>".$res['quantity']."</td>";
+  echo "<td>".$res['description']."</td>";	
 
 
-	<table width='80%' border=0>
+  		
+}
+?>
 
-	<tr bgcolor='#CCCCCC'>
-		<td>Categary</td>
-		<td>quantity</td>
-		<td>description</td>
-		
-		<td>Update</td>
-	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-		while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['categary']."</td>";
-		echo "<td>".$res['quantity']."</td>";
-		echo "<td>".$res['description']."</td>";	
+								
+							</tbody>
+						</table>
+					</div>
 			
-	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
-	?>
-	</table>
+				
+
 </body>
+<!--===============================================================================================-->	
+<script src="table/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="table/vendor/bootstrap/js/popper.js"></script>
+	<script src="table/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="table/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="table/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 </html>
